90a268bb5c0ff990362f8b6a6823454c5d909b1c
Haider Muhammad <70658014+HADO564@users.noreply.github.com>
2024-04-27 00:18:10 +0500

    basic response handling done. Python scripts needed for further testing

--- routes/rec_handle.py
+++ routes/rec_handle.py
@@ -1,22 +1,17 @@
-#create a function on a route that receives mp4 data in a data stream, not a whole mp4 file at once and supplies it to an api and awaits a response from said api.
-
 from flask import Flask, request
-import requests
 import json
-
-port = 5000
-
 app = Flask(__name__)
 
 @app.route('/rec', methods=['POST'])
 def rec():
-    data = request.stream.read()
+    data = request.get_data()
     url = 'http://localhost:5001/rec'
     headers = {'Content-Type': 'application/json'}
-    
-    #instead of sending the data, print the details of the data being received in the stream
-    print(data)
-    
 
+    # instead of sending the data, print the details of the data being received in the stream
+    print(json.loads(data))
     # response = requests.post(url, data=data, headers=headers)
-    # return response.text
+    return data
+
+if __name__ == "__main__":
+    app.run(debug=True)
